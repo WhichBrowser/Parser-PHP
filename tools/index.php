03eb49632a613c52f4fd0dae1dc314b903cb9f2c
Niels Leenheer <niels@leenheer.nl>
2013-07-03 13:07:07 +0200

    Add some minimalistic style to the test tool

--- tools/index.php
+++ tools/index.php
@@ -6,37 +6,81 @@
 		<title>Useragents</title>
 		
 		<style>
-			body {
-				padding: 40px;
-			}
 		
+			html {
+				font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", "Helvetica", "Arial", "Lucida Grande", sans-serif;
+				font-size: 1.1em;
+			}
+
+			body {
+				background: #d9d9d9;
+				text-align: left;
+			}
+			
+			header {
+				width: 70%;
+				margin: 100px auto 50px;
+				padding: 30px;
+			}
+
+			a {
+				color: #0092bf;
+				text-decoration: none;
+			}
+			
+			a:hover {
+				text-decoration: underline;
+			}
+
+		
+			input,
+			textarea {
+				font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", "Helvetica", "Arial", "Lucida Grande", sans-serif;
+				font-size: 14pt;
+				border: none;
+				padding: 10px;
+			}
+
 			input[type=text] {
 				width: 100%;
-				font-size: 12pt;
 			}
 			
 			textarea {
-				margin: 10px 0;
-				font-size: 12pt;
 				width: 100%;
 				height: 100px;
+				margin: 10px 0;
+			}
+
+			input[type=submit] {
+				background: #0092bf;
+				color: #fff;
+				cursor: pointer;
+				width: 100px;
+				font-size: 13px;
 			}
 			
 			hr {
-				margin: 40px -40px;
+				border: none;
+				border-top: 1px solid #bbb;
+				margin: 40px 0;
 			}
+			
+			pre {
+				text-align: left;
+			}
+			
 		
 		</style>
 	</head>
 	
 	<body>
 		<header>
-			<form action='index.php' method='post'>
+			<form action='' method='post'>
 				<div>
-					<input type='text' name='useragent' value='<?php echo isset($_REQUEST['useragent']) ? $_REQUEST['useragent'] : ''; ?>'>
+					<input type='text' name='useragent' placeholder='UA string' value='<?php echo isset($_REQUEST['useragent']) ? $_REQUEST['useragent'] : ''; ?>'>
 				</div>
 				<div>
-					<textarea name='headers'><?php echo isset($_REQUEST['headers']) ? $_REQUEST['headers'] : ''; ?></textarea>
+					<textarea name='headers' placeholder='Headers'><?php echo isset($_REQUEST['headers']) ? $_REQUEST['headers'] : ''; ?></textarea>
 				</div>
 				
 				<input type='submit' value='Detect'>
@@ -48,6 +92,9 @@
 			<div>
 				<pre><?php 
 				
+					error_reporting(E_ALL);
+					ini_set('display_errors', true);
+
 					include('../libraries/whichbrowser.php');
 		
 					$headers = array();
@@ -63,14 +110,16 @@
 						}
 					}
 					
-					$detected = new WhichBrowser($headers);
+					if (count($headers)) {
+						$detected = new WhichBrowser($headers);
 										
-					echo "{\n";
-					if (isset($detected->browser)) echo "\tbrowser: " . htmlentities(json_encode($detected->browser)) . "\n"; 
-					if (isset($detected->engine)) echo "\tengine: " . htmlentities(json_encode($detected->engine)) . "\n"; 
-					if (isset($detected->os)) echo "\tos: " . htmlentities(json_encode($detected->os)) . "\n"; 
-					if (isset($detected->device)) echo "\tdevice: " . htmlentities(json_encode($detected->device)) . "\n"; 
-					echo "}";
+						echo "{\n";
+						if (isset($detected->browser)) echo "\tbrowser: " . htmlentities(json_encode($detected->browser)) . "\n"; 
+						if (isset($detected->engine)) echo "\tengine: " . htmlentities(json_encode($detected->engine)) . "\n"; 
+						if (isset($detected->os)) echo "\tos: " . htmlentities(json_encode($detected->os)) . "\n"; 
+						if (isset($detected->device)) echo "\tdevice: " . htmlentities(json_encode($detected->device)) . "\n"; 
+						echo "}";
+					}
 				
 				?></pre>
 			</div>
