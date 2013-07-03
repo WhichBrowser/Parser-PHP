ac9957b0cf7e7c0a84a40a07e3b8caff5b93b9a7
Niels Leenheer <niels@leenheer.nl>
2013-07-03 09:32:27 +0200

    Update device database

--- data/models-tizen.php
+++ data/models-tizen.php
@@ -1,6 +1,7 @@
 <?php
 
 		DeviceModels::$TIZEN_MODELS = array(
+			'Baltic'									=> array( 'Samsung', '"Baltic"' ),
 			'GT-I8800!'									=> array( 'Samsung', '"Melius"' ),
 			'GT-I8805!'									=> array( 'Samsung', '"Redwood"' ),
 			'GT-I9500!'									=> array( 'Samsung', 'GT-I9500 prototype' ),
