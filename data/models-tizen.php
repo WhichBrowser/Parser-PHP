f6d3809e11f061c13029abcdb0370d9877a134ae
Niels Leenheer <niels@leenheer.nl>
2013-07-10 22:32:05 +0200

    New device models

--- data/models-tizen.php
+++ data/models-tizen.php
@@ -5,5 +5,6 @@
 			'GT-I8800!'									=> array( 'Samsung', '"Melius"' ),
 			'GT-I8805!'									=> array( 'Samsung', '"Redwood"' ),
 			'GT-I9500!'									=> array( 'Samsung', 'GT-I9500 prototype' ),
+			'SM-Z9005!'									=> array( 'Samsung', 'SM-Z9005 prototype' ),
 			'SEC SC-001'								=> array( 'Samsung', 'SC-001 prototype' ),
 		);
