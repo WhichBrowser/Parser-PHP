1abaa5824e0bee6b12a4b34376642e9a273d78c6
Niels Leenheer <niels@leenheer.nl>
2013-08-18 14:59:24 +0200

    Add new Android browser ids

--- data/id-android.php
+++ data/id-android.php
@@ -1,12 +1,17 @@
 <?php
 
 		BrowserIds::$ANDROID_BROWSERS = array(
-//			com.android.browser
-//			com.google.android.browser
+			
+			/* Stock browser */
+			// com.android.browser
+			// com.google.android.browser
 
-//			com.hydrastream.hydroid
-//			com.oupeng.browser
+			/* Manufacturer browsers */
+			'com.lenovo.lps.simple'								=> 'Lenovo Browser',
+			'com.sonymobile.smallbrowser'						=> 'Sony Small Browser',
+			'com.zte.browser'									=> 'ZTE Browser',
 
+			/* Browsers */
 			'com.qihoo.browser'									=> '360 Phone Browser',
 			'net.adgjm.angel'									=> 'Angel Browser',
 			'com.apc.browser'									=> 'APC',
@@ -24,32 +29,43 @@
 			'com.dolphin.browser.lab.en'						=> 'Dolphin Beta',
 			'com.dolphin.browser.cn'							=> 'Dolphin Mini',
 			'com.dolphin.browser.en'							=> 'Dolphin Mini',
+			'com.dolphin.browser.xf'							=> 'Dolphin (Fighter Edition)',
 			'easy.browser.classic'								=> 'Easy Browser',
 			'easy.browser.com'									=> 'Easy Browser',
 			'it.blogspot.fedeveloper'							=> 'Emergency Browser',	
+			'org.espier.browser'								=> 'Espier Browser',	
+			'com.exsoul'										=> 'Exsoul Web Browser',	
 			'gpc.myweb.hinet.net.PopupWeb'						=> 'Floating Browser Flux',
 			'galaxy.browser.gb.free'							=> 'Galaxy Browser',
 			'galaxy.browser.gb.pro'								=> 'Galaxy Browser',
 			'jp.ddo.pigsty.Habit_Browser'						=> 'Habit Browser',
+			'jp.co.lunascape.android.ilunascape'				=> 'iLunascape 2',
+			'nu.tommie.inbrowser.beta'							=> 'InBrowser Beta',
 			'com.adsi.kioware.client.mobile.app'				=> 'KioWare Kiosk',
 			'com.vng.android.zingbrowser'						=> 'Laban',
+			'com.light.browser'									=> 'Light Browser',
 			'acr.browser.barebones'								=> 'Lightning Browser',
+			'acr.browser.lightning'								=> 'Lightning Browser+',
 			'com.mx.browser'									=> 'Maxthon',
 			'com.mx.browser.tablet'								=> 'Maxthon',
 			'com.fevdev.nakedbrowser'							=> 'Naked Browser',
 			'com.nhn.android.search'							=> 'Naver',
 			'com.access_company.android.nflifebrowser.lite'		=> 'NetFront LifeBrowser',
+			'com.jiubang.browser'								=> 'Next Browser',
 			'com.ninesky.browser'								=> 'Ninesky',
 			'com.compal.android.browser'						=> 'Ocean Browser',
 			'com.tencent.ibibo.mtt'								=> 'One Browser',
 			'info.guardianproject.browser'						=> 'Orweb',	
 			'com.appsverse.photon'								=> 'Photon Browser',
+			'com.tencent.mtt'									=> 'QQ Browser',
 			'com.tencent.international.mtt'						=> 'QQ Browser',
 			'com.tencent.mobileqq'								=> 'QQ Browser',
 			'com.tencent.padbrowser'							=> 'QQ Browser',
+			'sogou.mobile.explorer'								=> 'Sogou Mobile',
 			'com.skyfire.consumer.browser'						=> 'Skyfire',
 			'jp.co.fenrir.android.sleipnir'						=> 'Sleipnir',
 			'jp.co.fenrir.android.sleipnir_test'				=> 'Sleipnir',
+			'iron.web.jalepano.browser'							=> 'Super Fast Browser',
 			'com.mns.android.swing'								=> 'Swing Browser',
 			'org.tint'											=> 'Tint Browser',
 			'com.UCMobile'										=> 'UC Browser',
@@ -59,9 +75,32 @@
 			'sui.mRelease'										=> 'xScope Browser Pro',
 			'org.zirco'											=> 'Zirco Browser',
 			
+			/* Other apps */
+			'com.duckduckgo.mobile.android'						=> 'DuckDuckGo',
 			'com.jv.falcon.pro'									=> 'Falcon Pro',
 			'com.noinnion.android.greader.readerpro'			=> 'gReader Pro',
+			'com.baidu.hao123.global'							=> 'Hao123',
+			'com.personal.personalmovil'						=> 'Personal Movil',
 			'com.levelup.touiteur'								=> 'Plume',
+			'bdmobile.android.app'								=> 'Baidu',
 			'com.andrewshu.android.redditdonation'				=> 'Reddit is fun',
-			'com.sina.weibo'									=> 'Sina Weibo'
+			'com.sina.weibo'									=> 'Sina Weibo',
+			'com.tencent.mm'									=> 'WeChat',
+			
+			'com.phikod.deviceportinfo'							=> 'Device Info Android'
+			
+			/* Unknown ids */
+			// com.hydrastream.hydroid
+			// com.oupeng.browser
+			// com.guokr.android
+			// com.iflytek.elpmobile.app.smartbook
+			// com.intoetv.test
+			// com.example.webviewdemo
+			// com.tiantianmini.android.browser
+			// com.playjam.webplayer
+			// com.lxc.lvxingzhe
+			// com.fiteng.kioskapp4
+			// com.bic.menu
+			// cn.zhui.client589066
+			// ch.inspectron.yoojis.webbrowser
 		);
