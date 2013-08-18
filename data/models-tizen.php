0e343d37d7aca96a049ada13dc98f4184467be17
Niels Leenheer <niels@leenheer.nl>
2013-08-18 15:00:50 +0200

    Many new devices and a couple corrections

--- data/models-tizen.php
+++ data/models-tizen.php
@@ -5,6 +5,7 @@
 			'GT-I8800!'									=> array( 'Samsung', '"Melius"' ),
 			'GT-I8805!'									=> array( 'Samsung', '"Redwood"' ),
 			'GT-I9500!'									=> array( 'Samsung', 'GT-I9500 prototype' ),
-			'SM-Z9005!'									=> array( 'Samsung', 'SM-Z9005 prototype' ),
+			'SGH-N099'									=> array( 'Samsung', 'SGH-N099 prototype' ),		// NTT docomo variant?
+			'(ARMV7 )?SM-Z9005!'						=> array( 'Samsung', 'SM-Z9005 prototype' ),
 			'SEC SC-001'								=> array( 'Samsung', 'SC-001 prototype' ),
 		);
