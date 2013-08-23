8330b8e4919feff9630fd5b8cd6fd460991a9e3b
Niels Leenheer <niels@leenheer.nl>
2013-08-24 00:49:14 +0200

    Expand new UC UA header detection

--- libraries/whichbrowser.php
+++ libraries/whichbrowser.php
@@ -174,9 +174,84 @@
 		}
 
 		function analyseNewUCUserAgent($ua) {
+			if (preg_match('/pr\(UCBrowser\/([0-9\.]+)/', $ua, $match)) {
+				$this->browser->name = 'UC Browser';
+				$this->browser->version = new Version(array('value' => $match[1], 'details' => 2));
+				$this->browser->stock = false;
+			}		
+
+			/* Find os */
+			if (preg_match('/pf\(Linux\)/', $ua) && preg_match('/ov\(Android ([0-9\.]+)/', $ua, $match)) {
+				$this->os->name = 'Android';
+				$this->os->version = new Version(array('value' => $match[1]));
+			}		
+			
+			if (preg_match('/pf\(Symbian\)/', $ua) && preg_match('/ov\(S60V5/', $ua)) {
+				if (!isset($this->os->name) || $this->os->name != 'Series60') {
+					$this->os->name = 'Series60';
+					$this->os->version = new Version(array('value' => 5));
+				}
+			}
+
+			if (preg_match('/pf\(Windows\)/', $ua) && preg_match('/ov\(wds ([0-9\.]+)/', $ua, $match)) {
+				if (!isset($this->os->name) || $this->os->name != 'Windows Phone') {
+					$this->os->name = 'Windows Phone';
+
+					switch($match[1]) {
+						case '7.0':		$this->os->version = new Version(array('value' => '7.0')); break;
+						case '7.1':		$this->os->version = new Version(array('value' => '7.5')); break;
+						case '8.0':		$this->os->version = new Version(array('value' => '8.0')); break;
+					}
+				}
+			}		
+
+			if (preg_match('/pf\((?:42|44)\)/', $ua) && preg_match('/ov\((?:iPh OS )?(?:iOS )?([0-9\_]+)/', $ua, $match)) {
+				if (!isset($this->os->name) || $this->os->name != 'iOS') {
+					$this->os->name = 'iOS';
+					$this->os->version = new Version(array('value' => str_replace('_', '.', $match[1])));
+				}
+			}
+
+			/* Find engine */
+			if (preg_match('/re\(AppleWebKit\/([0-9\.]+)/', $ua, $match)) {
+				$this->engine->name = 'Webkit';
+				$this->engine->version = new Version(array('value' => $match[1]));
+			}		
+
+			/* Find device */
 			if (isset($this->os->name) && $this->os->name == 'Android') {
 				if (preg_match('/dv\((.*)\s+Build/', $ua, $match)) {
 					$device = DeviceModels::identify('android', $match[1]);
+					
+					if ($device) {
+						$this->device = $device;
+					}
+				}		
+			}
+
+			if (isset($this->os->name) && $this->os->name == 'Series60') {
+				if (preg_match('/dv\((?:Nokia)?([^\)]*)\)/', $ua, $match)) {
+					$device = DeviceModels::identify('s60', $match[1]);
+					
+					if ($device) {
+						$this->device = $device;
+					}
+				}		
+			}
+
+			if (isset($this->os->name) && $this->os->name == 'Windows Phone') {
+				if (preg_match('/dv\(([^\)]*)\)/', $ua, $match)) {
+					$device = DeviceModels::identify('wp', substr(strstr($match[1], ' '), 1));
+					
+					if ($device) {
+						$this->device = $device;
+					}
+				}		
+			}
+
+			if (isset($this->os->name) && $this->os->name == 'iOS') {
+				if (preg_match('/dv\(([^\)]*)\)/', $ua, $match)) {
+					$device = DeviceModels::identify('ios', $match[1]);
 					
 					if ($device) {
 						$this->device = $device;
