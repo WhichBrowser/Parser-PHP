0f2630cd27418a9e31904ff49c981dab973c2b32
Niels Leenheer <niels@leenheer.nl>
2014-06-26 22:56:38 +0200

    Opera Mini 8 for iOS detection

--- libraries/whichbrowser.php
+++ libraries/whichbrowser.php
@@ -3475,7 +3475,7 @@
 					$this->browser->name = 'Opera Mobile';
 				}
 			}
-
+			
 			if (preg_match('/Opera/i', $ua)) {
 				$this->browser->stock = false;
 				$this->browser->name = 'Opera';
@@ -3565,6 +3565,11 @@
 				if ($this->device->type == '') {
 					$this->device->type = TYPE_DESKTOP;
 				}
+			}
+
+			if (preg_match('/OPiOS\/([0-9.]*)/', $ua, $match)) {
+				$this->browser->name = 'Opera Mini';
+				$this->browser->version = new Version(array('value' => $match[1], 'details' => 2 ));
 			}
 
 			if (preg_match('/Coast\/([0-9.]*)/', $ua, $match)) {
@@ -4520,7 +4525,7 @@
 			}
 
 			if (isset($this->os->name) && isset($this->browser->name)) {
-				if ($this->os->name == 'iOS' && $this->browser->name == 'Opera Mini') {
+				if ($this->os->name == 'iOS' && ($this->browser->name == 'Opera Mini' && $this->browser->version->toFloat() < 8)) {
 					$this->os->version = null;
 				}
 
