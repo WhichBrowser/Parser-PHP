dd0257261701fc050dd7b8981f2a187b30e4b18b
Samo Dekleva <samo@appresorts.com>
2014-11-05 14:25:27 +0100

    Resolves WhichBrowser/WhichBrowser#17

--- detect.php
+++ detect.php
@@ -90,7 +90,7 @@ var WhichBrowser = (function(){
 			if (this.device.type == 'desktop' && os && engine != '' && !device) return 'an unknown browser based on ' + engine + ' running on ' + os;
 			if (this.browser.stock && os && !device) return os;
 			if (this.browser.stock && engine != '' && !device) return 'an unknown browser based on ' + engine;
-			
+
 			return 'an unknown browser';
 		}
 	};
