682dcafbe587082dbe1e6e81b79c4d9aaad44b2f
Niels Leenheer <niels@leenheer.nl>
2015-11-12 11:08:02 +0100

    Use __DIR__ instead of dirname(__FILE__)

--- src/testrunner/src/bootstrap.php
+++ src/testrunner/src/bootstrap.php
@@ -11,13 +11,13 @@
 	if (strpos(__FILE__, DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "testrunner") !== false) $location = 'local';
 
 	switch($location) {
-		case 'installed': 	include_once(dirname(__FILE__) . '/../../../autoload.php');
-							include_once(dirname(__FILE__) . '/../../whichbrowser/src/parser.php');
+		case 'installed': 	include_once(__DIR__ . '/../../../autoload.php');
+							include_once(__DIR__ . '/../../whichbrowser/src/parser.php');
 							break;
 
 		case 'local':
-		case 'dist': 		include_once(dirname(__FILE__) . '/../../../vendor/autoload.php');
-							include_once(dirname(__FILE__) . '/../../whichbrowser/src/parser.php');
+		case 'dist': 		include_once(__DIR__ . '/../../../vendor/autoload.php');
+							include_once(__DIR__ . '/../../whichbrowser/src/parser.php');
 							break;
 
 		default:			echo "\033[0;31mCannot determine what kind of environment we are running in. Aborted!\033[0m\n\n";
