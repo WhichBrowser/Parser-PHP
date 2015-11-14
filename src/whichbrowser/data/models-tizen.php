d879596276359f7d033a85f71cd9e991cabf2f63
Niels Leenheer <niels@leenheer.nl>
2015-11-14 17:22:58 +0100

    New Tizen devices

--- src/whichbrowser/data/models-tizen.php
+++ src/whichbrowser/data/models-tizen.php
@@ -11,15 +11,33 @@
 			'GT-I9500!'									=> [ 'Samsung', 'GT-I9500 prototype' ],
 			'SGH-N099'									=> [ 'Samsung', 'SGH-N099 prototype' ],
 			'(ARMV7 )?SM-Z9005!'						=> [ 'Samsung', 'SM-Z9005 prototype' ],
+			'Mobile-RD-PQ'								=> [ 'Samsung', 'RD-PQ prototype' ],
+			'TM1'										=> [ 'Samsung', 'TM1 prototype' ],
 			'SM-Z130!'									=> [ 'Samsung', 'Z1' ],
+			'TIZEN SM-Z130!'							=> [ 'Samsung', 'Z1' ],
 			'SM-Z300!'									=> [ 'Samsung', 'Z3' ],
+			'TIZEN SM-Z300!'							=> [ 'Samsung', 'Z3' ],
 			'SM-Z500!'									=> [ 'Samsung', 'SM-Z500' ],
 			'SM-Z700!'									=> [ 'Samsung', 'SM-Z700' ],
 			'SM-Z900!'									=> [ 'Samsung', 'Z' ],	
-			'SM-Z910!'									=> [ 'Samsung', 'SM-Z910 prototype' ],
+			'SM-Z910!'									=> [ 'Samsung', 'Z' ],
+			'Z3 Z910F'									=> [ 'Samsung', 'Z' ],
 			'SEC SC-001'								=> [ 'Samsung', 'SC-001 prototype' ],
 			'SEC SC-03F'								=> [ 'Samsung', 'ZeQ' ],						// Unreleased version for DoCoMo
 			'SC-03F'									=> [ 'Samsung', 'ZeQ' ],						// Unreleased version for DoCoMo
 
-			'SM-R750!'									=> [ 'Samsung', 'Gear S', TYPE_WATCH ]
+			'SM-R750!'									=> [ 'Samsung', 'Gear S', TYPE_WATCH ],
+
+			'NX300'										=> [ 'Samsung', 'NX300', TYPE_CAMERA ],
+
+			'hawkp'										=> [ 'Samsung', '"Hawkp"', TYPE_TELEVISION ],
+
+			'xu3'										=> [ 'Hardkernel', 'ODROID-XU3 developer board' ],
+
+//			'FamilyHub'									=> [ 'Samsung', 'FamilyHub' ],
+
+			'sdk'										=> [ null, null, TYPE_EMULATOR ],
+			'Emulator'									=> [ null, null, TYPE_EMULATOR ],
+			'Mobile-Emulator'							=> [ null, null, TYPE_EMULATOR ],
+			'TIZEN Emulator'							=> [ null, null, TYPE_EMULATOR ],
 		];
