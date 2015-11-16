37c7727ea6a0e585075dfc1d5d8016a8df8e972b
Niels Leenheer <niels@leenheer.nl>
2015-11-16 19:10:01 +0100

    Allow browsers without a name and just a 'using' to collapse to a string

--- src/parser/src/primitives.php
+++ src/parser/src/primitives.php
@@ -106,6 +106,7 @@
 			if (!empty($this->family)) $result['family'] = $this->family->toArray();
 			if (!empty($this->version)) $result['version'] = $this->version->toArray();
 
+			if (isset($result['name']) && empty($result['name'])) unset($result['name']);
 			if (isset($result['version']) && !count($result['version'])) unset($result['version']);
 
 			return $result;
@@ -120,6 +121,7 @@
 			if (!empty($this->name)) $result['name'] = $this->name;
 			if (!empty($this->version)) $result['version'] = $this->version->toArray();
 
+			if (isset($result['name']) && empty($result['name'])) unset($result['name']);
 			if (isset($result['version']) && !count($result['version'])) unset($result['version']);
 
 			return $result;
@@ -138,6 +140,7 @@
 			if (!empty($this->alias)) $result['alias'] = $this->alias;
 			if (!empty($this->version)) $result['version'] = $this->version->toArray();
 
+			if (isset($result['name']) && empty($result['name'])) unset($result['name']);
 			if (isset($result['version']) && !count($result['version'])) unset($result['version']);
 
 			return $result;
