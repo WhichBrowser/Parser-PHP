6343ddcdcd69b4f1d392caa670d115771c1c2024
Niels Leenheer <niels@leenheer.nl>
2015-11-28 18:52:30 +0100

    Refactoring step 10

---
+++ src/parser/src/Constants/DeviceSubType.php
@@ -0,0 +1,11 @@
+<?php
+
+
+	namespace WhichBrowser\Constants;
+
+	class DeviceSubType {
+		const FEATURE = 'feature';
+		const SMART = 'smart';
+		const CONSOLE = 'console';
+		const PORTABLE = 'portable';
+	}
