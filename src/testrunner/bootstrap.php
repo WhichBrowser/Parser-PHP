ca2a08cdedbd0eacedf58b8ec1e44a87550fbb26
Niels Leenheer <niels@leenheer.nl>
2015-11-29 17:49:22 +0100

    Make the testrunner PSR-1, PSR-2

--- src/testrunner/bootstrap.php
+++ src/testrunner/bootstrap.php
@@ -1,17 +1,11 @@
 <?php
-	
 
-	include_once 'src/testrunner.php';
-	include_once 'src/polyfills.php';
+include_once 'src/Testrunner.php';
+include_once 'src/Polyfills.php';
 
-
-	// Installed by Composer in the vendor directory
-	if (strpos(__FILE__, DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR) !== false) {
-		include_once __DIR__ . '/../../autoload.php';
-	}
-
-	// Development environment
-	else {
-		include_once __DIR__ . '/../../vendor/autoload.php';
-		include_once __DIR__ . '/../parser/bootstrap.php';
-	}
+if (strpos(__FILE__, DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR) !== false) {
+    include_once __DIR__ . '/../../autoload.php';
+} else {
+    include_once __DIR__ . '/../../vendor/autoload.php';
+    include_once __DIR__ . '/../parser/bootstrap.php';
+}
--- src/testrunner/runner.php
+++ src/testrunner/runner.php
@@ -1,83 +1,73 @@
 <?php
 
-	include_once 'bootstrap.php';
+include_once 'bootstrap.php';
 
-	use WhichBrowser\Testrunner;
+use WhichBrowser\Testrunner;
 
+set_error_handler(function ($errno, $errstr, $errfile, $errline, array $errcontext) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
 
-
-
-	function handleError($errno, $errstr, $errfile, $errline, array $errcontext) {
-	    // error was suppressed with the @-operator
-	    if (0 === error_reporting()) {
-	        return false;
-	    }
-
-	    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-	}
-	set_error_handler("handleError");
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
 
 
-	$all = false;
-	$command = 'compare';
-	$files = array();
 
-	array_shift($argv);
+$all = false;
+$command = 'compare';
+$files = [];
 
-	if (count($argv)) {
-		if (in_array($argv[0], array('compare', 'check', 'rebase', 'list'))) {
-			$command = array_shift($argv);
-		}
+array_shift($argv);
 
-		if (count($argv)) {
-			foreach($argv as $file) {
-				if (fnmatch("*.yaml", $file)) {
-					$files[] = $file;
-				}
-				else {
-					$files = array_merge($files, glob(dirname(__FILE__) . "/data/{$file}/*.yaml"));
-				}
-			}
-		}
+if (count($argv)) {
+    if (in_array($argv[0], [ 'compare', 'check', 'rebase', 'list' ])) {
+        $command = array_shift($argv);
+    }
 
-		else {
-			$files = glob(dirname(__FILE__) . "/data/*/*.yaml");
-		}
-	}
-	else {
-		$files = glob(dirname(__FILE__) . "/data/*/*.yaml");
-	}
+    if (count($argv)) {
+        foreach ($argv as $file) {
+            if (fnmatch("*.yaml", $file)) {
+                $files[] = $file;
+            } else {
+                $files = array_merge($files, glob(dirname(__FILE__) . "/data/{$file}/*.yaml"));
+            }
+        }
+    } else {
+        $files = glob(dirname(__FILE__) . "/data/*/*.yaml");
+    }
+} else {
+    $files = glob(dirname(__FILE__) . "/data/*/*.yaml");
+}
 
 
-	switch($command) {
+switch ($command) {
+    case 'list':
+        Testrunner::search($files);
+        break;
 
-		case 'list':
-				Testrunner::search($files);
-				break;
+    case 'check':
+        $result = Testrunner::compare($files);
 
-		case 'check':
-				$result = Testrunner::compare($files);
+        if (!$result) {
+            echo "\033[0;31mTestrunner failed, please fix or rebase before building or deploying!\033[0m\n\n";
+            exit(1);
+        }
 
-				if (!$result) {
-					echo "\033[0;31mTestrunner failed, please fix or rebase before building or deploying!\033[0m\n\n";
-					exit(1);
-				}
+        break;
 
-				break;
+    case 'compare':
+        $result = Testrunner::compare($files, true);
 
-		case 'compare':
-				$result = Testrunner::compare($files, true);
+        if (!$result) {
+            echo "\033[0;31mTestrunner failed, please look at runner.log for the details!\033[0m\n\n";
+            exit(1);
+        }
 
-				if (!$result) {
-					echo "\033[0;31mTestrunner failed, please look at runner.log for the details!\033[0m\n\n";
-					exit(1);
-				}
+        break;
 
-				break;
-
-		case 'rebase':
-				Testrunner::rebase($files, !$all);
-				break;
-	}
-
-
+    case 'rebase':
+        Testrunner::rebase($files, !$all);
+        break;
+}
