e794b78f3ddeabb513894ccaf3bfbf8af750cfe6
Niels Leenheer <niels@leenheer.nl>
2015-12-01 21:27:24 +0100

    Improve testrunner to provide code coverage

--- src/testrunner/runner.php
+++ src/testrunner/runner.php
@@ -18,26 +18,27 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline, array $errconte
 $all = false;
 $command = 'compare';
 $files = [];
+$options = [];
 
 array_shift($argv);
 
 if (count($argv)) {
-    if (in_array($argv[0], [ 'compare', 'check', 'rebase', 'list' ])) {
-        $command = array_shift($argv);
-    }
-
-    if (count($argv)) {
-        foreach ($argv as $file) {
-            if (fnmatch("*.yaml", $file)) {
-                $files[] = $file;
+    foreach ($argv as $argument) {
+        if (in_array($argument, [ 'compare', 'check', 'rebase', 'list' ])) {
+            $command = $argument;
+        } elseif (substr($argument, 0, 2) == '--') {
+            $options[] = substr($argument, 2);
+        } else {
+            if (fnmatch("*.yaml", $argument)) {
+                $files[] = $argument;
             } else {
-                $files = array_merge($files, glob(dirname(__FILE__) . "/data/{$file}/*.yaml"));
+                $files = array_merge($files, glob(dirname(__FILE__) . "/data/{$argument}/*.yaml"));
             }
         }
-    } else {
-        $files = glob(dirname(__FILE__) . "/data/*/*.yaml");
     }
-} else {
+}
+
+if (count($files) === 0) {
     $files = glob(dirname(__FILE__) . "/data/*/*.yaml");
 }
 
@@ -48,10 +49,28 @@ switch ($command) {
         break;
 
     case 'check':
+        if (in_array('coverage', $options)) {
+            $coverage = new PHP_CodeCoverage;
+            $coverage->start('Testrunner');
+        }
+
         $result = Testrunner::compare($files);
+
+        if (in_array('coverage', $options)) {
+            $coverage->stop();
+
+            $writer = new PHP_CodeCoverage_Report_Clover;
+            $writer->process($coverage, 'clover.xml');
+        }
 
         if (!$result) {
             echo "\033[0;31mTestrunner failed, please fix or rebase before building or deploying!\033[0m\n\n";
+
+            if (in_array('show', $options)) {
+                echo file_get_contents('runner.log') . "\n\n";
+                echo "Done!\n\n";
+            }
+
             exit(1);
         }
 
