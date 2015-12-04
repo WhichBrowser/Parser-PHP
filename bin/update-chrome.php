ff209f3da291f9cded64bd12db691d84046c91b5
Niels Leenheer <niels@leenheer.nl>
2015-12-04 07:42:44 +0100

    Add tooling

---
+++ bin/update-chrome.php
@@ -0,0 +1,59 @@
+<?php
+
+	include __DIR__ . '/../src/Data/Chrome.php';
+	include __DIR__ . '/../data/browsers-chrome.php';
+
+
+	$stable = [
+		'desktop' => [],
+		'mobile'  => []
+	];
+
+
+	$omaha = explode("\n", file_get_contents("http://omahaproxy.appspot.com/history"));
+	foreach ($omaha as $i => $line) {
+		$items = explode(",", $line);
+
+		if ($items[0] == 'mac' && $items[1] == 'stable') {
+			$stable['desktop'][] = implode('.', array_slice(explode('.', $items[2]), 0, 3));
+		}
+
+		if ($items[0] == 'android' && $items[1] == 'stable') {
+			$stable['mobile'][] = implode('.', array_slice(explode('.', $items[2]), 0, 3));
+		}
+	}
+
+	$stable['desktop'] = array_unique($stable['desktop']);
+	$stable['mobile'] = array_unique($stable['mobile']);
+
+	sort($stable['desktop']);
+	sort($stable['mobile']);
+
+
+	foreach ($stable['desktop'] as $i => $version) {
+		if (!isset(WhichBrowser\Data\Chrome::$DESKTOP[$version])) {
+			WhichBrowser\Data\Chrome::$DESKTOP[$version] = 'stable';
+		}
+	}
+
+	foreach ($stable['mobile'] as $i => $version) {
+		if (!isset(WhichBrowser\Data\Chrome::$MOBILE[$version])) {
+			WhichBrowser\Data\Chrome::$MOBILE[$version] = 'stable';
+		}
+	}
+
+
+	$result  = "";
+	$result .= "<?php\n\n";
+	$result .= "namespace WhichBrowser\Data;\n\n";
+	$result .= "Chrome::\$DESKTOP = [\n";
+	foreach (WhichBrowser\Data\Chrome::$DESKTOP as $version => $channel) $result .= "    '{$version}' => '{$channel}',\n";
+	$result .= "];\n\n";
+	$result .= "Chrome::\$MOBILE = [\n";
+	foreach (WhichBrowser\Data\Chrome::$MOBILE as $version => $channel) $result .= "    '{$version}' => '{$channel}',\n";
+	$result .= "];\n";
+
+
+	file_put_contents(__DIR__ . '/../data/browsers-chrome.php', $result);
+
+	echo `git diff {__DIR__}/../src/parser/data/browsers-chrome.php`;
