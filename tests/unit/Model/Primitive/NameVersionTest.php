3a12a5433871a52264d80465a04dd93b8fe52aff
Niels Leenheer <niels@leenheer.nl>
2015-12-05 21:09:34 +0100

    Improve unit test coverage

--- tests/unit/Model/Primitive/NameVersionTest.php
+++ tests/unit/Model/Primitive/NameVersionTest.php
@@ -107,8 +107,22 @@ class NameVersionTest extends PHPUnit_Framework_TestCase
 
         $nameversion->reset();
 
-        $nameversion->identifyVersion('/Version\/([0-9\.]+)/u', 'Version/1.0', [ 'details' => 1]);
+        $nameversion->identifyVersion('/Version\/([0-9\.]+)/u', 'Version/1.0', [ 'details' => 1 ]);
 
         $this->assertEquals('1', $nameversion->getVersion());
+
+
+        $nameversion->reset();
+
+        $nameversion->identifyVersion('/Version\/([0-9\.]+)/u', 'Version/1_0_2', [ 'type' => 'underscore' ]);
+
+        $this->assertEquals('1.0.2', $nameversion->getVersion());
+
+
+        $nameversion->reset();
+
+        $nameversion->identifyVersion('/Version\/([0-9\.]+)/u', 'Version/1.02', [ 'type' => 'legacy' ]);
+
+        $this->assertEquals('1.0.2', $nameversion->getVersion());
     }
 }
