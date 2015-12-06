3d256a9c6da68a9168635aa90d516bf0e438219f
Niels Leenheer <niels@leenheer.nl>
2015-12-06 13:23:32 +0100

    Remove unreachable code

--- src/Model/Engine.php
+++ src/Model/Engine.php
@@ -26,10 +26,6 @@ class Engine extends NameVersion
             $result['version'] = $this->version->toArray();
         }
 
-        if (isset($result['name']) && empty($result['name'])) {
-            unset($result['name']);
-        }
-
         if (isset($result['version']) && !count($result['version'])) {
             unset($result['version']);
         }
