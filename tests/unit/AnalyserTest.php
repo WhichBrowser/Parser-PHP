562eb824ff1a0dfe8af1e231618534d298194566
Niels Leenheer <niels@leenheer.nl>
2015-12-08 18:33:25 +0100

    Add unit tests for internal APIs

---
+++ tests/unit/AnalyserTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace WhichBrowserTest;
+
+use PHPUnit_Framework_TestCase;
+use WhichBrowser\Analyser;
+use WhichBrowser\Model\Main;
+
+/**
+ * @covers WhichBrowser\Parser
+ */
+class AnalyserTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreatingAnalyserGetData()
+    {
+        $analyser = new Analyser("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+
+        $this->assertTrue($analyser instanceof \WhichBrowser\Analyser);
+        
+        $analyser->analyse();
+
+        $data = $analyser->getData();
+
+        $this->assertTrue($data instanceof \WhichBrowser\Model\Main);
+    }
+
+    public function testCreatingAnalyserSetData()
+    {
+        $analyser = new Analyser("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+
+        $this->assertTrue($analyser instanceof \WhichBrowser\Analyser);
+        
+        $input = new Main();
+
+        $this->assertTrue($input instanceof \WhichBrowser\Model\Main);
+
+        $analyser->setData($input);
+
+        $analyser->analyse();
+
+        $output = $analyser->getData();
+
+        $this->assertTrue($output instanceof \WhichBrowser\Model\Main);
+    }
+}
---
+++ tests/unit/ParserTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace WhichBrowserTest;
+
+use PHPUnit_Framework_TestCase;
+use WhichBrowser\Parser;
+
+/**
+ * @covers WhichBrowser\Parser
+ */
+class ParserTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreatingParserWithString()
+    {
+        $parser = new Parser("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+
+        $this->assertTrue($parser instanceof \WhichBrowser\Parser);
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
+
+    public function testCreatingParserWithHeaders()
+    {
+        $parser = new Parser([
+            'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)'
+        ]);
+
+        $this->assertTrue($parser instanceof \WhichBrowser\Parser);
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
+
+    public function testCreatingParserWithOptions()
+    {
+        $parser = new Parser([
+            'headers' => [
+                'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)'
+            ]
+        ]);
+
+        $this->assertTrue($parser instanceof \WhichBrowser\Parser);
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
+}
