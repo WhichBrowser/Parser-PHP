93088eb821984214fde1b9f0dbc9be0a1911160f
Niels Leenheer <niels@leenheer.nl>
2015-12-18 11:58:44 +0100

    Add support for Sharp Zaurus PDAs

--- src/Analyser/Header/Useragent/Device.php
+++ src/Analyser/Header/Useragent/Device.php
@@ -5,10 +5,11 @@ namespace WhichBrowser\Analyser\Header\Useragent;
 trait Device
 {
     use Device\Cars, Device\Gaming, Device\Ereader, Device\Mobile,
-        Device\Media, Device\Television, Device\Signage;
+        Device\Media, Device\Television, Device\Signage, Device\Desktop;
 
     private function &detectDevice($ua)
     {
+        $this->detectDesktop($ua);
         $this->detectCars($ua);
         $this->detectEreader($ua);
         $this->detectGaming($ua);
