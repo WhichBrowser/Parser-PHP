0a15ae75dabe25e4476b7847ed7e668eebe58cfa
Niels Leenheer <niels@leenheer.nl>
2016-01-09 04:51:11 +0100

    Add Slackware version number

--- src/Analyser/Header/Useragent/Os.php
+++ src/Analyser/Header/Useragent/Os.php
@@ -1645,6 +1645,10 @@ trait Os
 
             if (preg_match('/Slackware/u', $ua)) {
                 $this->data->os->name = 'Slackware';
+                if (preg_match('/Slackware[ \/](1[0-9.]+)/u', $ua, $match)) {
+                    $this->data->os->version = new Version([ 'value' => $match[1] ]);
+                }
+
                 $this->data->device->type = Constants\DeviceType::DESKTOP;
             }
 
