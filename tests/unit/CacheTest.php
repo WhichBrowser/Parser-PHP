443ef8785b4ef6dcec7127fed373abfc4261d856
Niels Leenheer <niels@leenheer.nl>
2016-02-21 14:22:23 +0100

    Add a method to enable caching results using a PSR-6 cache pool

---
+++ tests/unit/CacheTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace WhichBrowserTest;
+
+use PHPUnit_Framework_TestCase;
+use WhichBrowser\Parser;
+use Cache\Adapter\PHPArray\ArrayCachePool;
+
+/**
+ * @covers WhichBrowser\Parser
+ */
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreatingParserWithoutArgumentsAndCallAnalyse()
+    {
+        function countCachedItems($pool) {
+            $items = 0;
+
+            $reflector = new \ReflectionClass($pool);
+            if ($reflector->hasProperty('cache')) {
+                $property = $reflector->getProperty('cache');
+                $property->setAccessible(true);
+                $items = count($property->getValue($pool));
+            }
+
+            return $items;
+        }
+
+        $pool = new ArrayCachePool();
+
+        $this->assertEquals(0, countCachedItems($pool));
+
+        $parser = new Parser();
+        $parser->setCache($pool);
+        $parser->analyse("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+        $result = $parser->toArray();
+
+        $this->assertEquals(1, countCachedItems($pool));
+
+        $parser = new Parser();
+        $parser->setCache($pool);
+        $parser->analyse("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+
+        $this->assertEquals($result, $parser->toArray());
+    }
+}
