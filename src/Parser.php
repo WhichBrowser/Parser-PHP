a4f6e15a6ce72b75a25752329412691ceacfb06a
Niels Leenheer <niels@leenheer.nl>
2016-02-21 11:19:24 +0100

    Allow Parser to be created without arguments and call analyse manually

--- src/Parser.php
+++ src/Parser.php
@@ -12,10 +12,17 @@ class Parser extends Main
      * @param  array|string   $headers   An array with all of the headers or a string with just the User-Agent header
      */
 
-    public function __construct($headers)
+    public function __construct($headers = null)
     {
         parent::__construct();
 
+        if (!is_null($headers)) {
+            $this->analyse($headers);
+        }
+    }
+
+    public function analyse($headers)
+    {
         $analyser = new Analyser($headers);
         $analyser->setdata($this);
         $analyser->analyse();
--- tests/unit/ParserTest.php
+++ tests/unit/ParserTest.php
@@ -41,4 +41,15 @@ class ParserTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
     }
+
+    public function testCreatingParserWithoutArgumentsAndCallAnalyse()
+    {
+        $parser = new Parser();
+        $parser->analyse("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
+        
+        $this->assertTrue($parser instanceof \WhichBrowser\Parser);
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
+
 }
