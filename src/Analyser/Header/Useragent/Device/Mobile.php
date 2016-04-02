0ff616d86a5aeb045e9414df89da6459cf4a8a50
Niels Leenheer <niels@leenheer.nl>
2016-04-02 22:47:27 +0200

    One more optimisation

--- src/Analyser/Header/Useragent/Device/Mobile.php
+++ src/Analyser/Header/Useragent/Device/Mobile.php
@@ -249,6 +249,10 @@ trait Mobile
             return;
         }
 
+        if (!preg_match('/(T-Mobile|Danger|HPiPAQ|Acer|Amoi|AIRNESS|ASUS|BenQ|maui|ALCATEL|Bird|COOLPAD|CELKON|Coship|Cricket|DESAY|Diamond|dopod|Ericsson|FLY|GIONEE|HIKe|Hisense|HS|HTC|T[0-9]{4,4}|HUAWEI|Karbonn|KWC|KONKA|KTOUCH|K-Touch|Lenovo|Lephone|LG|Micromax|MOT|Nexian|NEC|NGM|OPPO|Panasonic|Pantech|Philips|Sanyo|Sam|SEC|SGH|SCH|SIE|Sony|SE|SHARP|Spice|Tecno|T-smart|TCL|Tiphone|Toshiba|UTStar|vk|Vodafone|Xiaomi|ZTE)/ui', $ua)) {
+            return;
+        }
+
         $this->data->device->identifyModel('/T-Mobile[_ ]([^\/;]+)/u', $ua, [
             'type'          => Constants\DeviceType::MOBILE,
             'manufacturer'  => 'T-Mobile'
