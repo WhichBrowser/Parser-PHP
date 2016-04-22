e4aab512ae0738fc94ad2b3689f4c8aaaca56fca
Niels Leenheer <niels@leenheer.nl>
2016-04-22 12:40:04 +0200

    Improve detection of Archos 5 with Android

--- src/Analyser/Header/Useragent/Device/Media.php
+++ src/Analyser/Header/Useragent/Device/Media.php
@@ -3,6 +3,7 @@
 namespace WhichBrowser\Analyser\Header\Useragent\Device;
 
 use WhichBrowser\Constants;
+use WhichBrowser\Model\Version;
 
 trait Media
 {
@@ -20,10 +21,12 @@ trait Media
 
 
 
-    /* Archos Generation 4 and 5 */
+    /* Archos Generation 4, 5 and 6 */
 
     private function detectArchos($ua)
     {
+        /* Generation 4 */
+
         if (preg_match('/Archos A([67]04)WIFI\//u', $ua, $match)) {
             $this->data->os->reset();
             $this->data->device->setIdentification([
@@ -33,6 +36,8 @@ trait Media
             ]);
         }
 
+        /* Generation 5 */
+
         if (preg_match('/ARCHOS; GOGI; a([67]05f?);/u', $ua, $match)) {
             $this->data->os->reset();
             $this->data->device->setIdentification([
@@ -41,6 +46,8 @@ trait Media
                 'type'          =>  Constants\DeviceType::MEDIA
             ]);
         }
+
+        /* Generation 6 without Android */
 
         if (preg_match('/ARCHOS; GOGI; G6([SHL]);/u', $ua, $match)) {
             $this->data->os->reset();
@@ -58,6 +65,23 @@ trait Media
                     $this->data->device->model = '7';
                     break;
             }
+        }
+
+        /* Generation 6 with Android */
+
+        if (preg_match('/ARCHOS; GOGI; A5[SH]; Version ([0-9]\.[0-9])/u', $ua, $match)) {
+            $version = new Version([ 'value' => $match[1] ]);
+
+            $this->data->os->reset([
+                'name'          => 'Android',
+                'version'       => new Version([ 'value' => $version->is('<', '1.7') ? '1.5' : '1.6' ])
+            ]);
+
+            $this->data->device->setIdentification([
+                'manufacturer'  =>  'Archos',
+                'model'         =>  '5',
+                'type'          =>  Constants\DeviceType::MEDIA
+            ]);
         }
     }
 
