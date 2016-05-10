e60a1e732334bcb3e9a6da1dde213f081ddc1172
Harmen Stoppels <harmenstoppels@gmail.com>
2016-05-10 13:30:27 +0200

    Add static constructors + tests

--- tests/unit/ParserTest.php
+++ tests/unit/ParserTest.php
@@ -40,10 +40,27 @@ class ParserTest extends PHPUnit_Framework_TestCase
     {
         $parser = new Parser();
         $parser->analyse("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)");
-        
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
+
+    public function testCreatingParserFromUserAgent()
+    {
+        $parser = Parser::fromUserAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)');
+
         $this->assertTrue($parser instanceof \WhichBrowser\Parser);
 
         $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
     }
 
+    public function testCreatingParserFromHeaderArray()
+    {
+        $parser = Parser::fromHeaders([
+            'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; InfoPath.1)'
+        ]);
+
+        $this->assertTrue($parser instanceof \WhichBrowser\Parser);
+
+        $this->assertTrue($parser->isBrowser('Internet Explorer', '=', '6.0'));
+    }
 }
