68d417a3013c201421973d8f364adf0eaafdc11b
Niels Leenheer <niels@creatype.nl>
2017-12-12 11:25:30 +0100

    Add SNOM IP phones

--- src/Analyser/Header/Useragent/Device/Phone.php
+++ src/Analyser/Header/Useragent/Device/Phone.php
@@ -11,19 +11,33 @@ trait Phone
     private function detectPhone($ua)
     {
         $this->detectNttTeless($ua);
+        $this->detectSnom($ua);
     }
 
 
 
 
 
-    /* Casio */
+    /* NTT Teless */
 
     private function detectNttTeless($ua)
     {
         if (preg_match('/Product\=NTT\/Teless/ui', $ua, $match)) {
             $this->data->device->manufacturer = 'NTT';
             $this->data->device->model = 'Teless';
+            $this->data->device->identified |= Constants\Id::MATCH_UA;
+            $this->data->device->type = Constants\DeviceType::MOBILE;
+            $this->data->device->subtype = Constants\DeviceSubType::DESKTOP;
+        }
+    }
+
+    /* SNOM */
+
+    private function detectSnom($ua)
+    {
+        if (preg_match('/snom(.+)-SIP/ui', $ua, $match)) {
+            $this->data->device->manufacturer = 'SNOM';
+            $this->data->device->model = $match[1] . ' IP Telephone';
             $this->data->device->identified |= Constants\Id::MATCH_UA;
             $this->data->device->type = Constants\DeviceType::MOBILE;
             $this->data->device->subtype = Constants\DeviceSubType::DESKTOP;
