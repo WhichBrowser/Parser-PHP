2c9b9d5656b3804d6ba2aed50dc9240840f896da
Niels Leenheer <niels@creatype.nl>
2017-12-16 15:33:32 +0100

    Add Yuanda phones

--- src/Analyser/Header/Useragent/Device/Mobile.php
+++ src/Analyser/Header/Useragent/Device/Mobile.php
@@ -249,7 +249,7 @@ trait Mobile
             return;
         }
 
-        if (!preg_match('/(T-Mobile|Danger|HPiPAQ|Acer|Amoi|AIRNESS|ASUS|BenQ|maui|ALCATEL|Bird|COOLPAD|CELKON|Coship|Cricket|DESAY|Diamond|dopod|Ericsson|FLY|GIONEE|GT-|Haier|HIKe|Hisense|HS|HTC|T[0-9]{4,4}|HUAWEI|Karbonn|KWC|KONKA|KTOUCH|K-Touch|Lenovo|Lephone|LG|Micromax|MOT|Nexian|NEC|NGM|OPPO|Panasonic|Pantech|Philips|Sagem|Sanyo|Sam|SEC|SGH|SCH|SIE|Sony|SE|SHARP|Spice|Tecno|T-smart|TCL|Tiphone|Toshiba|UTStar|Videocon|vk|Vodafone|Xiaomi|Zen|Ziox|ZTE|WAP)/ui', $ua)) {
+        if (!preg_match('/(T-Mobile|Danger|HPiPAQ|Acer|Amoi|AIRNESS|ASUS|BenQ|maui|ALCATEL|Bird|COOLPAD|CELKON|Coship|Cricket|DESAY|Diamond|dopod|Ericsson|FLY|GIONEE|GT-|Haier|HIKe|Hisense|HS|HTC|T[0-9]{4,4}|HUAWEI|Karbonn|KWC|KONKA|KTOUCH|K-Touch|Lenovo|Lephone|LG|Micromax|MOT|Nexian|NEC|NGM|OPPO|Panasonic|Pantech|Philips|Sagem|Sanyo|Sam|SEC|SGH|SCH|SIE|Sony|SE|SHARP|Spice|Tecno|T-smart|TCL|Tiphone|Toshiba|UTStar|Videocon|vk|Vodafone|Xiaomi|YUANDA|Zen|Ziox|ZTE|WAP)/ui', $ua)) {
             return;
         }
 
@@ -699,6 +699,11 @@ trait Mobile
             'manufacturer'  => 'Xiaomi'
         ]);
 
+
+        $this->data->device->identifyModel('/^YUANDA([0-9]+[A-Z]?)/ui', $ua, [
+            'type'          => Constants\DeviceType::MOBILE,
+            'manufacturer'  => 'Yuanda'
+        ]);
 
         $this->data->device->identifyModel('/^ZEN[_\s]([A-Z0-9\s\+]+)\*?[\s_]?($|\/|-|;|Dorado|MAUI|WAP|R2AE|Q03C)/uiU', $ua, [
             'type'          => Constants\DeviceType::MOBILE,
