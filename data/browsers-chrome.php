e28debd99ab8e5aff510e42e835af1ffe5d23b6b
Niels Leenheer <niels@creatype.nl>
2018-06-28 12:04:16 +0200

    Update Chrome

--- data/browsers-chrome.php
+++ data/browsers-chrome.php
@@ -71,6 +71,9 @@ Chrome::$DESKTOP = [
     '62.0.3202' => 'stable',
     '63.0.3239' => 'stable',
     '64.0.3282' => 'stable',
+    '65.0.3325' => 'stable',
+    '66.0.3359' => 'stable',
+    '67.0.3396' => 'stable',
 ];
 
 Chrome::$MOBILE = [
@@ -114,4 +117,7 @@ Chrome::$MOBILE = [
     '62.0.3202' => 'stable',
     '63.0.3239' => 'stable',
     '64.0.3282' => 'stable',
+    '65.0.3325' => 'stable',
+    '66.0.3359' => 'stable',
+    '67.0.3396' => 'stable',
 ];
