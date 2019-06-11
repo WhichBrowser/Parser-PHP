4e15d415590ef38ba6358bdfadb733700a0539a4
Niels Leenheer <niels@creatype.nl>
2019-06-11 09:43:55 +0200

    Add Vivo Browser

--- data/applications-browsers.php
+++ data/applications-browsers.php
@@ -167,6 +167,7 @@ Applications::$BROWSERS = [
         [ 'name' => 'Swing Browser',        'id'    => 'swing',      'regexp' =>'/Swing\(And\)\/([0-9.]*)/u', 'details' => 3 ],
         [ 'name' => 'Symphony',             'id'    => 'symphony',      'regexp' =>'/Symphony ([0-9.]+)$/u' ],
         [ 'name' => 'TinY',                 'id'    => 'ucpro',      'regexp' =>'/ucpro\/([0-9.]+)/iu' ],
+        [ 'name' => 'Vivo Browser',         'id'    => 'vivo',      'regexp' =>'/VivoBrowser\/([0-9.]+)/iu' ],
         [ 'name' => 'WebLite',              'id'    => 'weblite',      'regexp' =>'/WebLite\/([0-9.]*)/u', 'type' => Constants\DeviceType::MOBILE ],
 
         /* Television browsers */
