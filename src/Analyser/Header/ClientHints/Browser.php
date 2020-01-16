423c51d65812d2d2fc8dc14cace687d3fc282747
Niels Leenheer <niels@creatype.nl>
2020-01-16 13:37:45 -0700

    Add detection of channel based on chrome build version

--- src/Analyser/Header/ClientHints/Browser.php
+++ src/Analyser/Header/ClientHints/Browser.php
@@ -11,12 +11,26 @@ trait Browser
     private function &detectBrowser($ch)
     {
         if (preg_match('/Google Chrome ([0-9.]*)/u', $ch, $matches)) {
+            $version = $matches[1];
+            
             $this->data->browser->reset([
                 'name'      => 'Chrome',
                 'type'      => Constants\BrowserType::BROWSER,
                 'stock'     => false,
-                'version'   => new Version([ 'value' => $matches[1] ])
+                'version'   => new Version([ 'value' => $version ])
             ]);
+
+            if (preg_match('/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/u', $version)) {
+                $channel = Data\Chrome::getChannel('desktop', $version);
+
+                if ($channel == 'stable') {
+                    $this->data->browser->version->details = 1;
+                } elseif ($channel == 'beta') {
+                    $this->data->browser->channel = 'Beta';
+                } else {
+                    $this->data->browser->channel = 'Dev';
+                }
+            }
         }
 
         return $this;
