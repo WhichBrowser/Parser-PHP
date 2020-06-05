8a6299f8e04ea864fae3474d2ed34ba28dd6f3de
Ayumi Hamasaki <ayumi@github.com>
2020-06-05 15:10:01 +0100

    Juzi Browser

--- data/applications-browsers.php
+++ data/applications-browsers.php
@@ -139,6 +139,7 @@ Applications::$BROWSERS = [
         [ 'name' => 'iSurf',                'id'    => 'isurf',      'regexp' =>'/iSurf version \/v([0-9.]*)/u', 'details' => 2 ],
         [ 'name' => 'Jig Browser',          'id'    => 'jig browser',      'regexp' =>'/jig browser(?: web| core|9i?)?;? ?([0-9.]+)?/u', 'type' => Constants\DeviceType::MOBILE ],
         [ 'name' => 'Jumanji',              'id'    => 'jumanji',      'regexp' =>'/jumanji/u' ],                                                                         // see: jumanji browser ubuntu
+        [ 'name' => 'Juzi Browser',         'id'    => 'juzi',        'regexp' => '/JuziBrowser/iu', 'type' => Constants\DeviceType::DESKTOP ],                            // see: http://www.123juzi.com/
         [ 'name' => 'Kiosk',                'id'    => 'kiosk',      'regexp' =>'/Kiosk\/([0-9.]*)/u' ],                                                                // see: http://www.kioskbrowser.net
         [ 'name' => 'LeBrowser',            'id'    => 'lebrowser',      'regexp' =>'/LeBrowser\/([0-9.]*)/u', 'details' => 2 ],
         [ 'name' => 'LieBaoFast',           'id'    => 'liebao',      'regexp' =>'/LieBaoFast\/([0-9.]*)/u' ],                                                           // see: http://www.liebao.cn
