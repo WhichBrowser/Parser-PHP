2baa91189a7de04664f0045ca0e2d8b52e8f3e62
Ayumi Hamasaki <ayumi@github.com>
2020-08-07 19:13:44 +0100

    WhatsApp not being detected

--- data/applications-others.php
+++ data/applications-others.php
@@ -73,6 +73,7 @@ Applications::$OTHERS = [
         [ 'name' => 'Facebook Messenger',   'id'    => 'messenger',      'regexp' =>'/FB_IAB\/MESSENGER/u' ],
         [ 'name' => 'Kik',                  'id'    => 'kik',      'regexp' =>'/Kik\/([0-9.]*?)(\.0)?\.[0-9]{3,4}/u' ],
         [ 'name' => 'WeChat',               'id'    => 'messenger',      'regexp' =>'/MicroMessenger\/([0-9.]*)/u' ],
+        [ 'name' => 'WhatsApp',             'id'    => 'whatsapp',      'regexp' =>'/WhatsApp\/([0-9.]*)/u' ],
         [ 'name' => 'Wire',                 'id'    => 'wire',      'regexp' =>'/Wire\/([0-9.]*)/u' ],
         [ 'name' => 'Yahoo Messenger',      'id'    => 'yahoo',      'regexp' =>'/YahooMobile\/1.0 \(im; ([0-9.]+)\)/u', 'details' => 3 ],
         [ 'name' => 'Yammer',               'id'    => 'yammer',      'regexp' =>'/Yammer\/([0-9.]*)/u', 'details' => 2 ],
