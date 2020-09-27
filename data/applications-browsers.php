92f1a4e7ff1ddad1bb91b03c19dccb28c9b8243b
Ayumi Hamasaki <ayumi@github.com>
2020-09-27 13:46:23 +0100

    Update applications-browsers.php

--- data/applications-browsers.php
+++ data/applications-browsers.php
@@ -25,7 +25,7 @@ Applications::$BROWSERS = [
         [ 'name' => 'Acorn Browse',         'id'    => 'acorn',      'regexp' =>'/Acorn Browse ([0-9.]+)/u'  ],
         [ 'name' => 'Acorn Browse',         'id'    => 'acorn',      'regexp' =>'/Acorn Phoenix ([0-9.]+)/u'  ],
         [ 'name' => 'Acoo Browser',         'id'    => 'acoo',       'regexp' =>'/Acoo Browser/u' ],
-        [ 'name' => 'Alienforce',           'id'    => 'alienforce',  'regexp' => '/Alienforce(?:\/)([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                     // see: https://sourceforge.net/projects/alienforce/
+        [ 'name' => 'Alienforce',           'id'    => 'alienforce',  'regexp' => '/Alienforce\/:?([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                     // see: https://sourceforge.net/projects/alienforce/
         [ 'name' => 'Amigo Browser',        'id'    => 'amigo',       'regexp' => '/ Amigo\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                       // see: https://amigo.mail.ru/
         [ 'name' => 'AOL Desktop',          'id'    => 'aol',      'regexp' =>'/AOL ([0-9.]*); AOLBuild/i' ],
         [ 'name' => 'AOL Browser',          'id'    => 'america',      'regexp' =>'/America Online Browser (?:[0-9.]*); rev([0-9.]*);/i' ],
