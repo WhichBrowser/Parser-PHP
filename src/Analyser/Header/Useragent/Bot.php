ef103e1af3e8cc75dee6abedd604bce414fb2342
Ayumi Hamasaki <ayumi@github.com>
2020-10-07 12:56:22 +0100

    Update Bot.php

--- src/Analyser/Header/Useragent/Bot.php
+++ src/Analyser/Header/Useragent/Bot.php
@@ -46,6 +46,8 @@ trait Bot
         if (preg_match('/Go-http-client\/([0-9.]*)/u', $ua, $result)) {
             $this->data->browser->name = 'Go Http Client';
             $this->data->browser->version = $result[1] ?? '';
+
+            $this->data->device->type = Constants\DeviceType::BOT;
         }
 
         return $this;
