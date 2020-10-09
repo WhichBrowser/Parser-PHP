d9387c9959ba0d13f36c642b5c0d6911b5e1cd01
Ayumi Hamasaki <ayumi@github.com>
2020-10-09 16:31:46 +0100

    MojeekBot

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -158,6 +158,7 @@ Applications::$BOTS = [
     [ 'name' => 'Microsoft Social Streams',     'id'    => 'socialstreams',      'regexp' => '/Microsoft MSN SocialStreams Bot/u' ],
     [ 'name' => 'Mixi',                         'id'    => 'mixi',      'regexp' => '/mixi-check\/([0-9.]*)/u' ],
     [ 'name' => 'MnoGoSearch',                  'id'    => 'mnogo',      'regexp' => '/MnoGoSearch\/([0-9.]*)/u' ],
+    [ 'name' => 'MojeekBot',                    'id'    => 'mojeek',      'regexp' => '/MojeekBot\/([0-9.]*)/u' ],
     [ 'name' => 'Monitor.us',                   'id'    => 'monitor',      'regexp' => '/monitor.us/u' ],
     [ 'name' => 'Moz',                          'id'    => 'dotbot',         'regexp' => '/DotBot\/([0-9.]*)/i' ],
     [ 'name' => 'Moz',                          'id'    => 'rogerbot',         'regexp' => '/rogerbot\/([0-9.]*)/i' ],
