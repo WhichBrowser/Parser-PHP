505636053016f37fbaf9f5788bcd448740f8656d
Ayumi Hamasaki <ayumi@github.com>
2020-10-17 12:43:20 +0100

    Re-add `!` also missed out `name:` in one of the tests

--- src/SearchEngines/Yahoo.php
+++ src/SearchEngines/Yahoo.php
@@ -29,76 +29,76 @@ class Yahoo
      */
     public function __construct($ua)
     {
-        /* Yahoo Slurp Bot 1 */
+        /* Yahoo! Slurp Bot 1 */
         if (preg_match('/Yahoo\! Slurp\/?([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Slurp Bot';
+            $this->name = 'Yahoo! Slurp Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Slurp Bot 2 */
+        /* Yahoo! Slurp Bot 2 */
         } elseif (preg_match('/Yahoo\! DE Slurp\/?([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Slurp Bot';
+            $this->name = 'Yahoo! Slurp Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Slurp China Bot */
+        /* Yahoo! Slurp China Bot */
         } elseif (preg_match('/Yahoo\! Slurp China\/?([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Slurp China Bot';
+            $this->name = 'Yahoo! Slurp China Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Cache System Bot */
+        /* Yahoo! Cache System Bot */
         } elseif (preg_match('/YahooCacheSystem/u', $ua, $match)) {
-            $this->name = 'Yahoo Cache System Bot';
+            $this->name = 'Yahoo! Cache System Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Japan Bot */
+        /* Yahoo! Japan Bot */
         } elseif (preg_match('/Y\!J-([a-zA-Z]+)\/([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Japan Bot';
+            $this->name = 'Yahoo! Japan Bot';
             $this->version = new Version([ 'value' => $match[2] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Seeker Testing Bot 1 */
+        /* Yahoo! Seeker Testing Bot 1 */
         } elseif (preg_match('/YahooSeeker(?:\/([0-9.]*))?/u', $ua, $match)) {
-            $this->name = 'Yahoo Seeker Testing Bot';
+            $this->name = 'Yahoo! Seeker Testing Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Seeker Testing Bot 2 */
+        /* Yahoo! Seeker Testing Bot 2 */
         } elseif (preg_match('/YahooSeeker-Testing\/v([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Seeker Testing Bot';
+            $this->name = 'Yahoo! Seeker Testing Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Seeker Testing Bot 3 */
+        /* Yahoo! Seeker Testing Bot 3 */
         } elseif (preg_match('/yahooseeker-jp-mobile/u', $ua, $match)) {
-            $this->name = 'Yahoo Seeker Testing Bot';
+            $this->name = 'Yahoo! Seeker Testing Bot';
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Link Preview Bot */
+        /* Yahoo! Link Preview Bot */
         } elseif (preg_match('/Yahoo Link Preview/u', $ua, $match)) {
-            $this->name = 'Yahoo Link Preview Bot';
+            $this->name = 'Yahoo! Link Preview Bot';
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Mail Proxy Bot */
+        /* Yahoo! Mail Proxy Bot */
         } elseif (preg_match('/YahooMailProxy/u', $ua, $match)) {
-            $this->name = 'Yahoo Mail Proxy Bot';
+            $this->name = 'Yahoo! Mail Proxy Bot';
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
 
-        /* Yahoo Image Bot */
+        /* Yahoo! Image Bot */
         } elseif (preg_match('/Yahoo-MMCrawler\/([0-9.]*)/u', $ua, $match)) {
-            $this->name = 'Yahoo Image Bot';
+            $this->name = 'Yahoo! Image Bot';
             $this->version = new Version([ 'value' => $match[1] ]);
             $this->bot = Constants\DeviceType::BOT;
             $this->found = true;
