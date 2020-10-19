eae6c7b5fb1635da07e6e3ac7613daac846f7200
Ayumi Hamasaki <ayumi@github.com>
2020-10-19 20:36:42 +0100

    Move yandex detection after the main bot detection

--- src/Analyser/Header/Useragent/Bot.php
+++ src/Analyser/Header/Useragent/Bot.php
@@ -32,6 +32,17 @@ trait Bot
             $this->data->device->type = Constants\DeviceType::BOT;
         }
 
+        /* Detect based on a predefined list or markers */
+
+        if ($bot = Data\Applications::identifyBot($ua)) {
+            $this->data->browser = $bot;
+            $this->data->os->reset();
+            $this->data->engine->reset();
+            $this->data->device->reset();
+
+            $this->data->device->type = Constants\DeviceType::BOT;
+        }
+
         /* Detect yandex search engine bots */
 
         if (preg_match('/yandex/iu', $ua, $match)) {
@@ -43,17 +54,6 @@ trait Bot
                 $this->data->browser->version = $Yandex->version ?? '';
                 $this->data->device->type = $Yandex->bot ?? '';
             }
-        }
-
-        /* Detect based on a predefined list or markers */
-
-        if ($bot = Data\Applications::identifyBot($ua)) {
-            $this->data->browser = $bot;
-            $this->data->os->reset();
-            $this->data->engine->reset();
-            $this->data->device->reset();
-
-            $this->data->device->type = Constants\DeviceType::BOT;
         }
 
         return $this;
