b800490c1f10461998143261e2e7865351135dd3
Niels Leenheer <niels@leenheer.nl>
2021-01-02 22:10:23 +0100

    Update testrunner to use updated code coverage library

--- bin/runner.php
+++ bin/runner.php
@@ -4,6 +4,8 @@ include_once __DIR__ . '/bootstrap.php';
 
 use WhichBrowser\Testrunner;
 use WhichBrowser\Tests;
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Report\Clover;
 
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     // error was suppressed with the @-operator
@@ -50,7 +52,7 @@ switch ($command) {
 
     case 'check':
         if (in_array('coverage', $options)) {
-            $coverage = new PHP_CodeCoverage;
+            $coverage = new CodeCoverage;
             $coverage->filter()->addDirectoryToWhitelist('src');
             $coverage->start('Testrunner');
         }
@@ -60,7 +62,7 @@ switch ($command) {
         if (in_array('coverage', $options)) {
             $coverage->stop();
 
-            $writer = new PHP_CodeCoverage_Report_Clover;
+            $writer = new Clover;
             $writer->process($coverage, 'runner.xml');
 
             echo "\nCoverage saved as runner.xml\n\n";
