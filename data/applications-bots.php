1cf9b598ed238af6203f20a735a09f637130daef
Niels Leenheer <niels@leenheer.nl>
2021-01-03 16:03:15 +0100

    Add `IstellaBot` I believe it is an Italian search engine - to the bots list (closing PR #571)

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -165,6 +165,7 @@ Applications::$BOTS = [
     [ 'name' => 'Ichiro',                       'id'    => 'ichiro',      'regexp' => '/ichiro\/([0-9.]*)/u' ],
     [ 'name' => 'Internet Archive',             'id'    => 'archive',      'regexp' => '/InternetArchive\/([0-9.]*)/u' ],
     [ 'name' => 'IndeedBot',                    'id'    => 'indeed',      'regexp' => '/IndeedBot ([0-9.]*)/u' ],
+    [ 'name' => 'Istellabot',                   'id'    => 'istellabot',      'regexp' => '/istellabot\/t\.([0-9.]*)/u' ],
     [ 'name' => 'JamesBOT',                     'id'    => 'crawler',      'regexp' => '/James BOT - WebCrawler/u' ],
     [ 'name' => 'Jayde',                        'id'    => 'crawler',      'regexp' => '/Jayde Crawler./u' ],
     [ 'name' => 'KakaoTalk Bot',                'id'    => 'kakao',              'regexp' => '/kakaotalk-scrap\/([0-9.]*)/u' ],
