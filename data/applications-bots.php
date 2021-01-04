ed27e0d78d12af7c5832882d3c1ed7fa83771d26
Niels Leenheer <niels@leenheer.nl>
2021-01-04 13:12:30 +0100

    Add `BuzzSumo` a popular social media / seo platform (closes PR #525)

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -65,6 +65,7 @@ Applications::$BOTS = [
     [ 'name' => 'Buck',                         'id'    => 'buck',      'regexp' => '/Buck\/([0-9.]*)/u' ],
     [ 'name' => 'BuiltWith',                    'id'    => 'builtwith',      'regexp' => '/BuiltWith\/([0-9.]*)/u' ],
     [ 'name' => 'Butterfly',                    'id'    => 'butterfly',      'regexp' => '/Butterfly\/([0-9.]*)/u' ],
+    [ 'name' => 'BuzzSumo',                     'id'    => 'buzzsumo',      'regexp' => '/buzzsumo\.com/u' ],
     [ 'name' => 'Bytespider',                   'id'    => 'bytespider',      'regexp' => '/Bytespider/u' ],
     [ 'name' => 'CiteSeerX',                    'id'    => 'heritrix',      'regexp' => '/heritrix\/([0-9.]*)/u' ],
     [ 'name' => 'Clarabot',                     'id'    => 'clarabot',      'regexp' => '/Clarabot\/([0-9.]*)/u' ],
