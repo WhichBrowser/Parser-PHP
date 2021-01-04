566d0165c2954867c3606e3c7d26a9c292d2cb9f
Niels Leenheer <niels@leenheer.nl>
2021-01-04 16:00:57 +0100

    Add Google Favicon bot

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -147,6 +147,7 @@ Applications::$BOTS = [
     [ 'name' => 'Google App Engine',            'id'    => 'google',      'regexp' => '/AppEngine-Google/u' ],
     [ 'name' => 'Google Web Preview',           'id'    => 'google',      'regexp' => '/Google Web Preview/u' ],
     [ 'name' => 'Google Page Speed',            'id'    => 'google',      'regexp' => '/Google Page Speed Insights/u' ],
+    [ 'name' => 'Google Favicon',               'id'    => 'google',      'regexp' => '/Google Favicon/u' ],
     [ 'name' => 'Google FeedBurner',            'id'    => 'feed',      'regexp' => '/FeedBurner\/([0-9.]*)/u' ],
     [ 'name' => 'Google Feedfetcher',           'id'    => 'google',      'regexp' => '/Feedfetcher-Google/iu' ],
     [ 'name' => 'Google Font Analysis',         'id'    => 'google',      'regexp' => '/Google-FontAnalysis\/([0-9.]*)/u' ],
