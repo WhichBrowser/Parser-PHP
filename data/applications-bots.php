f7bf3d60b1a6db64fea18e3939374141e896ca3b
Niels Leenheer <niels@leenheer.nl>
2021-01-04 13:32:32 +0100

    Add `Line Spider` to bots list (closes PR #512)

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -186,6 +186,7 @@ Applications::$BOTS = [
     [ 'name' => 'Kouio',                        'id'    => 'kouio',      'regexp' => '/kouio.com/u' ],
     [ 'name' => 'Larbin',                       'id'    => 'larbin',      'regexp' => '/[Ll]arbin[\/_]([0-9.]*)/u' ],
     [ 'name' => 'LetsearchBot',                 'id'    => 'letsearch',      'regexp' => '/LetsearchBot\/([0-9.]*)/u' ],
+    [ 'name' => 'Line Bot',                     'id'    => 'line',        'regexp' => '/Linespider\/([0-9.]*)/u' ],
     [ 'name' => 'Linkedin Bot',                 'id'    => 'linkedin',      'regexp' => '/LinkedInBot\/([0-9.]*)/u' ],
     [ 'name' => 'Linkpad Bot',                  'id'    => 'linkpad',      'regexp' => '/LinkpadBot\/([0-9.]*)/u' ],
     [ 'name' => 'Linkdex Bot',                  'id'    => 'linkdex',      'regexp' => '/linkdexbot-mobile\/([0-9.]*)/u' ],
