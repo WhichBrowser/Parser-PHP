703170b3844d40e299f02648b729968cbf82868d
Ayumi <ayumih880@gmail.com>
2021-01-11 18:04:46 +0000

    Update tests

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -384,6 +384,8 @@ Applications::$BOTS = [
     [ 'name' => 'Node Fetch',                   'id'    => 'node',      'regexp' => '/node-fetch\/([0-9.]*)/u' ],
     [ 'name' => 'Wget',                         'id'    => 'wget',      'regexp' => '/Wget\/([0-9.]*)/u' ],
     [ 'name' => 'Curl',                         'id'    => 'curl',      'regexp' => '/curl\/([0-9.]*)/ui' ],
+    [ 'name' => 'Curl',                         'id'    => 'curl',      'regexp' => '/pycurl/ui' ],
+    [ 'name' => 'Libcurl Agent',                'id'    => 'curl',      'regexp' => '/libcurl-agent\/([0-9.]*)/ui' ],
 
     [ 'name' => 'Go',                           'id'    => 'package',      'regexp' => '/Go [0-9\.]+ package http/u' ],
     [ 'name' => 'Java',                         'id'    => 'java',      'regexp' => '/^Java\/([0-9.]*)/u' ],
