ef995b4d8cb0264d8b07829319d2594b158f120f
Ayumi <ayumih880@gmail.com>
2021-02-01 11:57:43 +0000

    Add Jetty web server (bots are using this server)

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -183,6 +183,7 @@ Applications::$BOTS = [
     [ 'name' => 'Istellabot',                   'id'    => 'istellabot',      'regexp' => '/istellabot\/t\.([0-9.]*)/u' ],
     [ 'name' => 'JamesBOT',                     'id'    => 'crawler',      'regexp' => '/James BOT - WebCrawler/u' ],
     [ 'name' => 'Jayde',                        'id'    => 'crawler',      'regexp' => '/Jayde Crawler./u' ],
+    [ 'name' => 'Jetty',                        'id'    => 'jetty',      'regexp' => '/Jetty\/([0-9.]*)/u' ],
     [ 'name' => 'KakaoTalk Bot',                'id'    => 'kakao',              'regexp' => '/kakaotalk-scrap\/([0-9.]*)/u' ],
     [ 'name' => 'Kouio',                        'id'    => 'kouio',      'regexp' => '/kouio.com/u' ],
     [ 'name' => 'Larbin',                       'id'    => 'larbin',      'regexp' => '/[Ll]arbin[\/_]([0-9.]*)/u' ],
