2fdda4351e9ac187e604e47fcea998d584f04ea8
Ayumi <ayumih880@gmail.com>
2021-02-01 13:48:09 +0000

    try this method

--- src/Analyser/Header/Useragent/Bot.php
+++ src/Analyser/Header/Useragent/Bot.php
@@ -25,8 +25,19 @@ trait Bot
         if (preg_match('/(?:Bot|Robot|Spider|Crawler)([\/\);]|$)/iu', $ua) && !preg_match('/CUBOT/iu', $ua)) {
             $this->data->browser->reset();
             $this->data->os->reset();
-            $this->data->engine->reset();
+            $this->version = new Version([ 'value' => $match[1] ]);
+
+            $this->data->device->type = Constants\DeviceType::BOT;
+        }
+
+        /* Detect Async Http Client bot */
+
+        if (preg_match('/^AHC\/([0-9.]+)/ui', $ua, $match)) {
+            $this->data->browser->reset();
             $this->data->device->reset();
+
+            $this->data->browser->name = 'Async Http Client';
+            $this->data->browser->version = new Version([ 'value' => $match[1] ]);
 
             $this->data->device->type = Constants\DeviceType::BOT;
         }
