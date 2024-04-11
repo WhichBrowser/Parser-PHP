adbf7bf537b04da02497225a11e880beebb44962
Roman Martinuk <romanmartinuk@gmail.com>
2024-04-11 14:55:02 +0300

    fix deprecated
    Deprecated: Creation of dynamic property WhichBrowser\Analyser\Header\Useragent::$data is deprecated

--- src/Analyser/Header/Useragent.php
+++ src/Analyser/Header/Useragent.php
@@ -6,13 +6,16 @@ class Useragent
 {
     use Useragent\Os, Useragent\Device, Useragent\Browser, Useragent\Application, Useragent\Using, Useragent\Engine, Useragent\Bot;
 
+    private $data;
+    private $options;
+
     public function __construct($header, &$data, &$options)
     {
         $this->data =& $data;
         $this->options =& $options;
 
         /* Make sure we do not have a duplicate concatenated useragent string */
-  
+
         $header = preg_replace("/^(Mozilla\/[0-9]\.[0-9].{20,})\s+Mozilla\/[0-9]\.[0-9].*$/iu", '$1', $header);
 
         /* Detect the basic information */
